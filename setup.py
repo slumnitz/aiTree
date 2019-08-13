c12642aea9d8925f46a12b6c07bd1d21b6de2d64
Stefanie Lumnitz <stefanie.lumnitz@gmail.com>
2019-08-12 22:48:07 -0700

    add setup.py to initiate package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='aitree',
+    version='0.0.1rc',
+    description='Mapping urban trees from street-level imagery',
+    long_description='',
+    url='',
+    author='Stefanie Lumnitz',
+    author_email='stefanie.lumnitz@gmail.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
